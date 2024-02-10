ea311e96f9e27c92c2f706fc6247e545756ec3f7
hanerach <vkruglov1337@yandex.ru>
2024-02-10 13:47:56 +0300

    homework_results

--- main.py
+++ main.py
@@ -195,6 +195,29 @@ def choice(planet_name):
                         </html>'''
 
 
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f'''<!doctype html>
+                            <html lang="en">
+                              <head>
+                                <meta charset="utf-8">
+                                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                               <link rel="stylesheet"
+                               href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                               integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                               crossorigin="anonymous">
+                                <title>Результаты</title>
+                              </head>
+                              <body>
+                                <h1>Результаты отбора</h1>
+                                <p>Претендента на участие в миссии {nickname}:</p>
+                                <div class="alert alert-success">Поздравляем! Ваш рейтинг после {level} этапа отбора</div>
+                                <p>составляет {rating}!</p>
+                                <div class="alert alert-warning">Желаем удачи!</div>
+                              </body>
+                            </html>'''
+
+
 
 if __name__ == '__main__':
     app.run(port=8001, host='127.0.0.1')
