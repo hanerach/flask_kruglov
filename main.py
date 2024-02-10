820b9d037f1377da414755b0052e6e571d373dce
hanerach <vkruglov1337@yandex.ru>
2024-02-10 12:56:42 +0300

    promotion_image

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
@@ -39,6 +39,33 @@ def image_mars():
           </body>
         </html>
     """
+
+
+@app.route('/promotion_image')
+def promotion_image():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                       <link rel="stylesheet"
+                       href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                       integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
+                       crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <p><img src="/static/img/mars.jpeg"></p>
+                        <div class="alert alert-secondary">Человечество вырастает из детства.</div>
+                        <div class="alert alert-success">Человечеству мала одна планета.</div>
+                        <div class="alert alert-secondary">Мы сделаем обитаемыми безжизненные пока планеты.</div>
+                        <div class="alert alert-warning">И начнём с Марса!</div>
+                        <div class="alert alert-danger">Присоединяйся!</div>
+                      </body>
+                    </html>'''
+
 
 if __name__ == '__main__':
     app.run(port=8001, host='127.0.0.1')
